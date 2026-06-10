0fecc08247aba6877cfec6f7c14285228343c6a3
harshbyte-art <odhiamboharshley@gmail.com>
2026-06-11 02:28:26 +0300

    Fix API function

--- api/token.ts
+++ api/token.ts
@@ -1,4 +1,6 @@
-export default async function handler(req: any, res: any) {
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') return res.status(405).end()
 
   const { code, codeVerifier, redirectUri, clientId } = req.body
