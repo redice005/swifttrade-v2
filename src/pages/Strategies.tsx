391550f48f9ffd836b2310d058a2f05ad504e7b0
redice005 <karewaharshley@gmail.com>
2026-08-05 15:48:19 +0300

    Update Strategies.tsx

--- src/pages/Strategies.tsx
+++ src/pages/Strategies.tsx
@@ -1,1 +1,11 @@
-null
+import NavBar from '../components/NavBar'
+
+export default function Strategies() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Strategies</h1>
+      <p>Trading strategies will appear here.</p>
+    </div>
+  )
+}
