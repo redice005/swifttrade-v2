e33052b712aa417d4f16c612972b5d3e5d197c33
redice005 <karewaharshley@gmail.com>
2026-08-21 08:40:30 +0300

    Update Bots.tsx

--- src/pages/Bots.tsx
+++ src/pages/Bots.tsx
@@ -643,6 +643,8 @@ export default function Bots() {
           style={{
             color: '#6c63ff',
             margin: 0,
+            fontSize:'1.4',
+            fontWeight:600
           }}
         >
           ⚡ Swift Trade
