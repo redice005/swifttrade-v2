d4c44772bb2fc616f34d728f552f00a4fd0f0698
redice005 <karewaharshley@gmail.com>
2026-08-23 13:59:47 +0300

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -972,7 +972,7 @@ export default function Dashboard() {
                   fontSize: '0.68rem'
                 }}
               >
-                This changes the displayed value only.This features is only available to the admin qnd not other users
+                This changes the displayed value only.This features is only available to the admin and not other users
               </p>
             </div>
           )}
